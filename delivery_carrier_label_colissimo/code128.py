ffe0652f514caa65736fcbddad3f54b29a2198ef
Sébastien BEAU <sebastien.beau@akretion.com>
2015-04-01 15:26:15 +0200

    [FIX] fix import

--- delivery_carrier_label_colissimo/code128.py
+++ delivery_carrier_label_colissimo/code128.py
@@ -14,8 +14,7 @@
 # ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 # OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE
 
-import Image
-import ImageDraw
+from PIL import Image, ImageDraw
 from base64 import encodestring
 from StringIO import StringIO
 
