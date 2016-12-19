22668c5a7be3c67c4b0cc6f00a94a47a99298c13
David Beal <david.beal@akretion.com>
2016-12-19 11:03:03 +0100

    IMP add seller country on view

--- delivery_roulier_coliposte/__openerp__.py
+++ delivery_roulier_coliposte/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Delivery Roulier Coliposte',
-    'version': '0.3',
+    'version': '1.0',
     'author': 'Akretion',
     'summary': "Direct label printing for ColiPoste International carrier",
     'maintainer': 'Akretion',
@@ -25,6 +25,7 @@ Contributors
     'website': 'http://www.akretion.com/',
     'data': [
         'data/delivery.xml',
+        'product_view.xml',
     ],
     'tests': [],
     'demo': [
@@ -35,7 +36,5 @@ Contributors
         ],
     },
     'installable': True,
-    'auto_install': False,
     'license': 'AGPL-3',
-    'application': False,
 }
